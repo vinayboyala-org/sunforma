365a6fcaa0e518dd8b32d3ae66398218992ebc7c
vinayboyala8 <47546853+vinayboyala8@users.noreply.github.com>
2019-02-15 18:14:40 +0530

    Update two.java

--- two.java
+++ two.java
@@ -1,4 +1,4 @@
 This file is created in Development branch you buddies.......keep it undeleted
 Modified the file in development branch.
 MOdified in Master branch
-Adding this to check git pull.
+Adding the line to check git pull.
